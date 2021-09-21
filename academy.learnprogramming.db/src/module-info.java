04474e833b00668e1e1c468fedb17a99b6383265
BuildTools <unconfigured@null.spigotmc.org>
2021-09-20 19:39:58 -0700

    Old, finished project music playlist with sqlite

---
+++ academy.learnprogramming.db/src/module-info.java
@@ -0,0 +1,9 @@
+module academy.learnprogramming.db {
+    requires java.sql;
+    requires sqlite.jdbc;
+    requires transitive academy.learnprogramming.common;
+
+
+    exports academy.learnprogramming.db;
+
+}
